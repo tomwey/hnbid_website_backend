4e874e375c3862b77ba080464dedadd17e37d087
tomwey <tomwey@163.com>
2018-12-03 17:11:58 +0800

    change configs

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "后台系统"
+  config.site_title = "合能招投标后台系统"
   
   config.namespace :admin do |admin|
     admin.build_menu do |menu|
@@ -32,7 +32,7 @@ ActiveAdmin.setup do |config|
   end
   
   # 自定义页脚
-  config.footer = "版权所有 #{Date.today.year}"
+  config.footer = "版权所有 #{Date.today.year} 合能招投标"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
